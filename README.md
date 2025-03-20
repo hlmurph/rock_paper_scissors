995edc9dbcea85a9ad7a7b52bbc73b6d6e0e0370
hlmurph <106940356+hlmurph@users.noreply.github.com>
2025-03-20 10:04:16 -0400

    Add function to play a round

--- README.md
+++ README.md
@@ -3,15 +3,37 @@ Javascript project for The Odin Project
 
 Pseudocode:
 
+Declare scores
+
+Create a function to play a round, taking human and computer choices as arguments
+    Make human choice case insensitive
+    Determine winner
+        For human rock
+            If computer is rock tie
+            If computer is paper lose
+            If computer is scissors win
+        For human paper
+            If computer is rock win
+            If computer is paper tie
+            If computer is scissors lose
+        For human scissors
+            If computer is rock lose
+            If computer is paper win
+            If computer is scissors tie
+    Print round result
+    Increment score
+
+
 Create a function to get the computer's move
     Create variable to hold choice
     Assign a random number from 1-3 to choice
     Assign a string to choice
-        rock if 1
-        paper if 2
-        scissors if 3
+        Rock if 1
+        Paper if 2
+        Scissors if 3
 
 Create a function to get the human's move
     Create a variable to hold the human's choice
     Prompt for human input
-    store human input in variable
+    Store human input in variable
+
--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+let humanScore = 0;
+let computerScore = 0;
+
+//Function to play a round
+function playRound(humanChoice, computerChoice) {
+    // Take human input and normalize it so only the first letter is capitalized
+    humanChoice = humanChoice.charAt(0).toUpperCase() + (humanChoice.toLowerCase).slice(1);
+    let result;
+    switch(humanChoice) {
+        case "Rock":
+            if (computerChoice == "Rock") {
+                result = "tie";
+            }
+            else if (computerChoice == "Paper") {
+                result = "lose";
+            }
+            else {
+                result = "win";
+            }       
+            break;
+        case "Paper":
+            if (computerChoice == "Rock") {
+                result = "win";
+            }
+            else if (computerChoice == "Paper") {
+                result = "tie";
+            }
+            else {
+                result = "lose";
+            }
+            break;
+        case "Scissors":
+            if (computerChoice == "Rock") {
+                result = "lose";
+            }
+            else if (computerChoice == "Paper") {
+                result = "win";
+            }
+            else {
+                result = "tie";
+            }
+            break;
+    }
+    if (result == "win") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore ++;
+    }
+    else if (result == "lose") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore ++;
+    }
+    else {
+        console.log(`Tie! You both chose ${humanChoice}`);
+    }
+}
+
 // Function to choose computer's move
 function getComputerChoice() {
     const num = (Math.floor(Math.random() * 3)) + 1;
@@ -16,6 +72,8 @@ function getComputerChoice() {
     return choice;
 }
 
+
+// Function to choose human's move
 function getHumanChoice() {
     return prompt("Choose Rock, Paper, or Scissors");
 }
