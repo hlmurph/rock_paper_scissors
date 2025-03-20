a0957c97cb2093de5973df925ce00952db7226c7
hlmurph <106940356+hlmurph@users.noreply.github.com>
2025-03-20 09:14:24 -0400

    Create function to make computer choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+function getComputerChoice () {
+    const num = (Math.floor(Math.random() * 3)) + 1;
+    let choice;
+    switch(num) {
+        case 1:
+            choice = "Rock";
+            break;
+        case 2:
+            choice = "Paper";
+            break;
+        case 3:
+            choice = "Scissors";
+            break;
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
